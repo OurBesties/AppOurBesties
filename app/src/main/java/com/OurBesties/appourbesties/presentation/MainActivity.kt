e0fb9ace836943aef29a417c1cad85dbdc6646fb
OurBesties <com.OurBesties@gmail.com>
2022-04-30 10:19:11 +0200

    Inicio del proyecto de la app móvil OurBesties

---
+++ app/src/main/java/com/OurBesties/appourbesties/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.OurBesties.appourbesties.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.OurBesties.appourbesties.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
